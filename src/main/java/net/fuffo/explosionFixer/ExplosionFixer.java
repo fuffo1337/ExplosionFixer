097f344ecbdf830c858aff6436a428bbf40070ec
federicotoluzzo <federico.toluzzo1@gmail.com>
2025-08-29 23:15:03 +0200

    Initial commit

---
+++ src/main/java/net/fuffo/explosionFixer/ExplosionFixer.java
@@ -0,0 +1,16 @@
+package net.fuffo.explosionFixer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExplosionFixer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
