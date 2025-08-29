bf88b94a209fa90362051e5a2711524438506fbb
federicotoluzzo <federico.toluzzo1@gmail.com>
2025-08-29 23:28:58 +0200

    Aggiunto il codice che effettivamente blocca le esplosioni. Yippie! Va! Ora è da controllare un fia'

--- src/main/java/net/fuffo/explosionFixer/ExplosionFixer.java
+++ src/main/java/net/fuffo/explosionFixer/ExplosionFixer.java
@@ -1,16 +1,40 @@
 package net.fuffo.explosionFixer;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.regen.TownyRegenAPI;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ExplosionFixer extends JavaPlugin {
+public final class ExplosionFixer extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        if (event.isCancelled()) return;
+
+        Location loc = event.getLocation();
+
+        // TODO: aggiungere condizioni più dettagliate così va in altri casi il plugin
+        if (true) {
+            TownBlock townBlock = TownyAPI.getInstance().getTownBlock(loc);
+            if (townBlock != null) {
+                TownyRegenAPI.cancelProtectionRegenTasks();
+            }
+        }
+    }
+
 }
